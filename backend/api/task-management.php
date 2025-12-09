e34c990cdd87d6d2f542dd390bc9d8ee5001d4f5
Dilip Gupta <developer@riseit.com>
2025-12-09 19:12:59 +0530

    Update View task 2

--- backend/api/task-management.php
+++ backend/api/task-management.php
@@ -21,13 +21,21 @@ switch ($method) {
 
     case 'GET':
         if($viewTask){
-            $sql1 = "SELECT t.id, u1.name AS assignedby, u2.name AS assignedto, t.deadline, t.remarks, t.status FROM tasks t INNER JOIN users u1 ON t.assigned_by = u1.id INNER JOIN users u2 ON t.assigned_to = u2.id;";
+            $sql1 = "SELECT t.id,t.task_name, u1.name AS assignedby, u2.name AS assignedto, t.deadline, t.remarks, t.status FROM tasks t INNER JOIN users u1 ON t.assigned_by = u1.id INNER JOIN users u2 ON t.assigned_to = u2.id;";
             $result = $conn->query($sql1);
             $data = [];
             while ($row = $result->fetch_assoc()) {
                 $data[] = $row;
             }
-            echo json_encode(["status" => "success","data" => $data]);
+
+            $sql2 = "SELECT * FROM users WHERE status='active'";
+            $result2 = $conn->query($sql2);
+            $staff = [];
+            while ($row2 = $result2->fetch_assoc()) {
+                $staff[] = $row2;
+            }
+
+            echo json_encode(["status" => "success","data" => $data, 'staff' => $staff]);
         }else{
             $sql1 = "SELECT * FROM users WHERE status='active'";
             $result = $conn->query($sql1);
@@ -60,6 +68,28 @@ switch ($method) {
         
         break;
 
+    case 'PUT':
+        $taskID=$_POST['task_id'] ?? '';
+        $taskName=$_POST['task_name'] ?? '';
+        $assignedBy = $_POST['assignedBy'] ?? '';
+        $email = $_POST['email'] ?? '';
+        $deadline = $_POST['deadline'] ?? '';
+        $status = $_POST['status'] ?? '';
+        $remarks = $_POST['remarks'] ?? '';
+        
+        $jsonAssignedTo = isset($_POST['assignedTo']) ? $_POST['assignedTo'] : '';
+        $assignedTos = json_decode($jsonAssignedTo, true);
+        $sql1 = "SELECT * From tasks where id='$taskID'";
+        $result1 = $conn->query($sql1);
+        if($result1 && $result1->num_rows > 0){
+            foreach ($assignedTos as $assignedTo) {
+                $sql1 = "UPDATE `tasks` SET `task_name`='$taskName',`assigned_to`='$assignedTo',`deadline`='$deadline',`status`='$status',`remarks`='$remarks',`updated_by`='$userId',`updated_date`='$date',`updated_time`='$time' WHERE ";
+                $conn->query($sql1);
+            }
+            echo json_encode(["status" => "success", "message" => "Task updated successfully!"]);
+        }else{
+            echo json_encode(["status" => "error", "message" => "Task not valid"]);    
+        }
 
     default:
         echo json_encode(["status" => "error", "message" => "Invalid request method"]);
